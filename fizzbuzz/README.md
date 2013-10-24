de80e613edb20f8dc71b614e243ee633cbd02011
Colin PUY <colin.puy@bonitasoft.com>
2013-10-24 14:24:25 +0200

    remove statement from test class

--- fizzbuzz/README.md
+++ fizzbuzz/README.md
@@ -2,7 +2,7 @@
 
 
 Given a number :
-* when number is not Fizz and not Buzz return the number 
+* return the number in string
 * when number contains a 3 then return Fizz
 * when number is a multiple of 3 then return Fizz
 * when number contains a 7 then return Buzz
--- fizzbuzz/src/test/java/org/bonitasoft/dojo/fizzbuzz/FizzBuzzerTest.java
+++ fizzbuzz/src/test/java/org/bonitasoft/dojo/fizzbuzz/FizzBuzzerTest.java
@@ -19,22 +19,6 @@ package org.bonitasoft.dojo.fizzbuzz;
 
 import org.junit.Before;
 
-/**
- * Given a number
- *  
- *  return the number in string
- *  
- *  when number contains a 3 then return Fizz
-
- *  when number is a multiple of 3 then return Fizz
- *  
- *  when number contains a 7 then return Buzz
- *  
- *  when number is a multiple of 7 then return Buzz
- *  
- *  when number is Fizz and Buzz then return FizzBuzz
- *  
- */
 public class FizzBuzzerTest {
 
     private FizzBuzzer fizzBuzzer;
